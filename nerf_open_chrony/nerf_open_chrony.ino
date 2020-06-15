cc9b50a74da5831cb733c39ddb1232cb18fca3ea
HL52840 <65380846+HL52840@users.noreply.github.com>
2020-06-15 16:20:41 -0700

    Commenting display functions.

--- nerf_open_chrony/nerf_open_chrony.ino
+++ nerf_open_chrony/nerf_open_chrony.ino
@@ -2,13 +2,13 @@
 #define serialdbgspd 9600 //Change to set serial debug speed (usually 9600, but you can change this to whatever you want.)
 #define bbgrams 0.2 //Nerf Dart or BB weight in grams for joules calculation.
 
-//DO NOT CHANGE ------------------------
+//DO NOT CHANGE ------------------------------------------------------------------------------------
 #define gate1pin 2 //Gate 1 input pin
 #define gate2pin 3 //Gate 2 input pin
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET 4 // Reset pin (or -1 if sharing Arduino reset pin)
-//--------------------------------------
+//--------------------------------------------------------------------------------------------------
 
 //Libraries Required
 #include <SPI.h>
@@ -24,31 +24,37 @@ float bbw = bbgrams; //BB weight (Grams)
 float joules = 0; //Joules
 float avgjoules = 0; //Average Joules
 float rof = 0; //Rate of fire
+
 long rof_t0 = 0;
 long rof_tprev = 0;
-int rof_count = 0;
+
 float avg_fps = 0; // Average Feet Per Second
-float shot_num = 0; //Shot Counter
-int disp_rot = 0; //Display Rotation (0 = Horizontal, 1 = Vertical)
+float shot_num = 0; //Shot 
+float gate_dist = 51.0; //distance in millimeters
+float fps = 0; //Frames Per Second
+
 long g1_time = 0; //Gate 1 recorded time
 long g2_time = 0; //Gate 2 recorded time
 long g1_slow_time = 0;
 long g2_slow_time = 0;
 long now = 0;
 long display_time = 0;
+
 bool g1_trip = false; //Gate 1 current state
 bool g2_trip = false; //Gate 2 current state
 bool g1_trip0 = false;
-bool  g2_trip0 = false;
+bool g2_trip0 = false;
 bool g1_trip_latch = false;
 bool g2_trip_latch = false;
+
+int disp_rot = 0; //Display Rotation (0 = Horizontal, 1 = Vertical)
+int rof_count = 0;
 int g1_persist = 0;
 int g2_persist = 0;
 int g1_pin = gate1pin; //Sensor Pin 0
 int g2_pin = gate2pin; //Sensor Pin 1
 int latch_persist = 2;
-float gate_dist = 51.0; //distance in millimeters
-float fps = 0; //Frames Per Second
+
 String fpstr = ""; //Frames per second in char form
 
 //Record Gate 1 time
@@ -70,87 +76,119 @@ void timer(){
 
 //Display vertical
 void display_vert(){
+
+  //Set rotation to vertical
   display.setRotation(1);
   display.clearDisplay();
   display.setTextSize(2);
-  
+
+  //Write feet per second label
   display.setCursor(12,10);
   display.print("FPS: ");
   display.setTextSize(2);
-  
+
+  //Write feet per second or error
   display.setCursor(12,30);
   display.print(fpstr);
   display.setTextSize(1);
-  
+
+  //Write number of shots
   display.setCursor(0,94);
   display.print("SHOT #:");
   display.print(int(shot_num));
-  
+
+  //Write average feet per second
   display.setCursor(0,81);
   display.print("AVGFPS:");
   display.print(int(avg_fps));
-  
+
+  //Write rounds per second
   display.setCursor(0,107);
   display.print("RND/S:");
   char buffer2[4];
   String srof = dtostrf(rof, 2, 1, buffer2);
   display.print(srof);
-  
+
+  //Write joules
   display.setCursor(0,55);
   display.print(joules);
   display.print("J");
 
+  //Write average joules
   display.setCursor(0,68);
   display.print(avgjoules);
   display.print("Jx");
-  
+
+  //Gate 1 indicator
   if(g1_trip){
     display.setCursor(0,119);
     display.print("G1");
   }
+
+  //Gate 2 indicator
   if(g2_trip){
     display.setCursor(15,119);
     display.print("G2");
   }
+
+  //Display everything for real
   display.display();
 }
 
 //Display horizontal
 void display_horz(){
+
+  //Set rotation to horizontal
   display.setRotation(0);
   display.clearDisplay();
   display.setTextSize(2);
+
+  //Write feet per second
   display.setCursor(12,12);
   display.print("FPS: ");
   display.print(fpstr);
   display.setTextSize(1);
+
+  //Write number of shots
   display.setCursor(42,46);
   display.print("SHOT # : ");
   display.print(int(shot_num));
+
+  //Write average feet per second
   display.setCursor(42,35);
   display.print("AVG FPS: ");
   display.print(int(avg_fps));
+
+  //Write rounds per second
   display.setCursor(42,57);
   display.print("RND/S  : ");
   char buffer[5];
   String srof = dtostrf(rof, 2, 1, buffer);
   display.print(srof);
+
+  //Write joules
   display.setCursor(2,35);
   display.print(joules);
   display.print("J");
 
+  //Write average joules
   display.setCursor(2,46);
   display.print(avgjoules);
   display.print("Jx");
-  
+
+  //Gate 1 indicator
   if(g1_trip){
     display.setCursor(2,57);
     display.print("G1");
   }
+
+  //Gate 2 indicator
   if(g2_trip){
     display.setCursor(18,57);
     display.print("G2");
   }
+
+  //Display everything for real
   display.display();
 }
 
